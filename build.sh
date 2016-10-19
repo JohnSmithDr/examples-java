6b26818b9803615a5cd364c76ab91a4da9a7b140
johnsmith1st <johnsmith1st@live.com>
2016-10-19 17:04:41 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
-echo Build helloworld-gradle
+echo '>> Build helloworld-gradle'
 cd helloworld-gradle
-gradle clean build
+gradle -q clean
+gradle -q build
+java -jar build/libs/helloworld-gradle.jar
 cd ..
 
-echo Build helloworld-maven
+echo '>> Build helloworld-maven'
 cd helloworld-maven
-mvn clean install
+mvn -q clean install
+java -jar target/helloworld-maven-1.0-SNAPSHOT.jar
+cd ..
+
+echo '>> Build multiproject-gradle'
+cd multiproject-gradle
+gradle -q clean
+gradle -q build
+gradle -q test
+gradle -q distZip
+gradle -q run
 cd ..
